f491450ab840baa35d8ba699a8c4dddb34908886
Jake Lussier <jake.t.lussier@gmail.com>
2019-01-07 11:20:19 -0500

    Added timezone handling and error handling.

--- sheettalk/server.py
+++ sheettalk/server.py
@@ -5,6 +5,7 @@ from dateutil import tz
 from flask import Flask, request, redirect
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import simplejson
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -14,9 +15,10 @@ def get_mapping_sheet(gc, fname):
         sh = gc.open(fname)
     except gspread.exceptions.SpreadsheetNotFound:
         sh = gc.create(fname)
-        if args.admin_email:
-            sh.share(args.admin_email, perm_type='user', role='writer')
+    if args.admin_email:
+        sh.share(args.admin_email, "user", "writer", notify=False)
     return sh.sheet1
+
 
 def set_mapping(mapping_sheet, number, url):
     numbers = mapping_sheet.col_values(1)
@@ -24,55 +26,63 @@ def set_mapping(mapping_sheet, number, url):
         row = numbers.index(number) + 1
     else:
         row = len(numbers) + 1
+    mapping_sheet.delete_row(row)
     mapping_sheet.insert_row([number, url], index=row)
+
 
 def process_message(number, message_body):
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(args.credentials, scope)
-    gc = gspread.authorize(credentials)
+    creds_dict = simplejson.load(open(args.credentials))
+    creds = ServiceAccountCredentials.from_json_keyfile_name(args.credentials,
+                                                             scope)
+    gc = gspread.authorize(creds)
     mapping_sheet = get_mapping_sheet(gc, "sheettalk mapping")
     # If the text is a spreadsheet url, save number -> url mapping.
     if message_body.startswith("http"):
-        try:
-            set_mapping(mapping_sheet, number, message_body)
-        except:
-            return "Could not set sheet".format(message_body)
-        return "Sheet now {}.".format(message_body)
+        set_mapping(mapping_sheet, number, message_body)
+        return('Spreadsheet set! Enter a column name, a space, and a value '
+               'to update the column with that time-stamped value.')
     # Otherwise, aim to update the user's sheet with the content.
     else:
-        # Get the current time.
-        tz_from = tz.gettz('UTC')
-        tz_to = tz.gettz('America/New_York')
-        time = datetime.utcnow().replace(tzinfo=tz_from).astimezone(tz_to)
-        time = time.strftime("%Y-%m-%d %H:%M:%S")
         # Get the sheet url and then the sheet.
         mapping = dict(mapping_sheet.get_all_values())
         try:
             url = mapping[number]
-        except:
-            return "Sheet not set for number {}".format(number)
+        except KeyError:
+            return('No spreadsheet set for {}. Text the url of the Google Sheet '
+                   'you would like to edit.'.format(number))
         try:
-            user_sheet = gc.open_by_url(url).sheet1
-        except:
-            return "Could not open user sheet {}".format(url)
+            user_spreadsheet = gc.open_by_url(url)
+            user_sheet = user_spreadsheet.sheet1
+        except gspread.exceptions.APIError:
+            return('Could not open spreadsheet. Make sure sheet is valid and '
+                   '"share" with {}.'.format(creds_dict["client_email"]))
+        # Get the user sheet time zonne and use it to set the current time.
+        properties = user_spreadsheet.fetch_sheet_metadata()["properties"]
+        tz_to = tz.gettz(properties["timeZone"])
+        tz_from = tz.gettz('UTC')
+        time = datetime.utcnow().replace(tzinfo=tz_from).astimezone(tz_to)
+        time = time.strftime("%Y-%m-%d %H:%M:%S")
         # Get the proper row and columns for the time and data.
-        try:
-            headers = [v.lower() for v in user_sheet.row_values(1)]
-            header, value = message_body.split(" ", 1)
-        except:
-            return "Could not parse user sheet {}".format(url)
+        headers = [v.lower() for v in user_sheet.row_values(1)]
+        header, value = message_body.split(" ", 1)
         if header.lower() not in headers:
-            return "{} not in sheet headers".format(header)
+            return('{} not in sheet headers.'.format(header))
         if "time" not in headers:
-            return "'time' not in sheet headers"
+            return('"time" not in sheet headers.')
         data_col = headers.index(header.lower()) + 1
         time_col = headers.index("time") + 1
         row = len(user_sheet.get_all_values()) + 1
         # Insert.
-        user_sheet.update_cell(row, time_col, time)
-        user_sheet.update_cell(row, data_col, value)
-        return "Updated {}".format(header)
+        try:
+            user_sheet.update_cell(row, time_col, time)
+            user_sheet.update_cell(row, data_col, value)
+        except gspread.exceptions.APIError:
+            return('Do not have permission to edit "{}." Please "share" with {}.'
+                   .format(properties["title"], creds_dict["client_email"]))
+        return('Added to column {}.'.format(header))
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
@@ -80,9 +90,10 @@ def sms_reply():
     try:
         response_body = process_message(request.form['From'], request.form['Body'])
     except Exception as e:
-        response_body = "Could not process. Error: {}".format(e)
+        response_body = "Could not process message"
+        print(e)
     # Construct and send TwiML response.
-    print("sending " + response_body)
+    print(response_body)
     response = MessagingResponse()
     response.message(response_body)
     return str(response)
